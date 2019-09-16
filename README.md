4833e4eb3a90c9cbc74d310279789c6ebca3e5e4
Rizwan <rizwanrensa@gmail.com>
2019-09-16 11:18:52 -0400

    added new page to post new campgrounds

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Yelp-Camping
+
+Layout and basics layout
+1. Create Basic header and footer
+2. Add in Bootstrap
+
+3.Creating new Campgrounds
+a.Setup new campground post route
+b. Add in body-parser
+c.Setup ROute to show form
+d.Add basic unstyled form
--- app.js
+++ app.js
@@ -1,16 +1,41 @@
 const express = require("express");
 const app = express();
+const path = require('path')
 const PORT = process.env.PORT || 3000
+const bodyParser = require('body-parser')
+//const hbs = require('hbs')
 
-app.set("view engine", "hbs");
+const viewPath = path.join(__dirname, './views')
+const partialsPath = path.join(__dirname, './views/partials')
+
+/*
+To handle HTTP POST request in Express.js version 4 and above, you need to install middleware module called body-parser.
+body-parser extract the entire body portion of an incoming request stream and exposes it on req.body.
+The middleware was a part of Express.js earlier but now you have to install it separately.
+This body-parser module parses the JSON, buffer, string and URL encoded data submitted using HTTP POST request. */
+
+let campgrounds = [{
+    name: 'Salmon Creek',
+    image: 'https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c73277dd59644c65c_340.png',
+  },
+  {
+    name: 'Mayburne Ave',
+    image: 'https://pixabay.com/get/5fe8d1434852b108f5d084609620367d1c3ed9e04e50744f7d297ad59e4ec1_340.jpg'
+  }
+]
 
 
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
-
-
-app.listen(PORT, () => {
-  console.log(`listening on ${PORT}`)
-})
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+//set the default layout to main.hbs - which in turn shows the landing and campgrounds page
+app.engine("handlebars", exphbs({
+  defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
 
 
 app.get('/', (req, res) => {
@@ -18,12 +43,35 @@ app.get('/', (req, res) => {
 })
 
 app.get('/campgrounds', (req, res) => {
-  let campgrounds = [{
-    name: 'Salmon Creek',
-    image: 'https:pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c73277ed69348c459_340.png'
-  }]
+
   //pass the campgrounds array as an object in the res.render method
   res.render('campgrounds', {
     campgrounds: campgrounds
   })
+})
+
+//when you hit the camgrounds/new route, it shows you the form, the form has action to post /campgrounds to where it post the data.
+app.get('/campgrounds/new', (req, res) => {
+  res.render('newcampgrounds')
+})
+
+
+app.post('/campgrounds', (req, res) => {
+  //{ name: 'asdasd', 'imageURL ': 'asdas' }
+  //we get this due to body-parser
+  console.log(req.body)
+  const name = req.body.name;
+  const imageURL = req.body.imageURL;
+
+
+  campgrounds.push({
+    name: name,
+    image: imageURL
+  })
+  res.redirect('/campgrounds')
+
+})
+
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`)
 })

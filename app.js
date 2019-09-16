8ee295c55faab439c745566b41ade016fe27296a
Rizwan <rizwanrensa@gmail.com>
2019-09-16 12:15:43 -0400

    setting up mysql with sequalize

--- app.js
+++ app.js
@@ -72,6 +72,19 @@ app.post('/campgrounds', (req, res) => {
 
 })
 
+// Requiring our models for syncing
+var db = require("./models");
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync().then(function () {
+  app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+  });
+});
+
+
+
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`)
 })
